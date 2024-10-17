86baa66919e5af404b6912017b330b7991df6bb2
Fedor Bokov <fedor@quasi.ai>
2024-10-16 23:44:01 -0400

    fixed finding of existing libopenvdb

--- openvdb_vendor/openvdb_vendor-extras.cmake
+++ openvdb_vendor/openvdb_vendor-extras.cmake
@@ -1,3 +1,5 @@
 list(APPEND CMAKE_MODULE_PATH "${openvdb_vendor_DIR}/../../../opt/openvdb_vendor/lib/cmake/OpenVDB")
+list(APPEND CMAKE_MODULE_PATH "/usr/local/lib/cmake/OpenVDB")
 list(APPEND CMAKE_MODULE_PATH "/usr/lib/x86_64-linux-gnu/cmake/OpenVDB")
+list(APPEND CMAKE_MODULE_PATH "/usr/lib/aarch64-linux-gnu/cmake/OpenVDB")
 find_package(OpenVDB REQUIRED)
